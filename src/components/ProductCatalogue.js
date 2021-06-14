575a2e23cf4b8992e64f1f1836fa3bb2b93eda39
viviane <v.buschwallace@yahoo.com>
2021-06-14 15:27:19 +0200

    cart in productcatalogue

--- src/components/ProductCatalogue.js
+++ src/components/ProductCatalogue.js
@@ -2,16 +2,30 @@ import React, { useState } from "react";
 import ProductItem from "./ProductItem";
 
 const ProductList = ({ data }) => {
-  // map through json data >>
+  // states >>
+  const [cart, setCart] = useState([]);
+
+  // add each item to cart with click >>
+  const addToCart = (item) => {
+    setCart((prevState) => [prevState, item]);
+  };
+
+  // map through json data to get each item >>
   const items = data.map((item, i) => (
     <ProductItem key={i} productInfo={item} />
   ));
 
   return (
     <React.Fragment>
-      <div className="cart-wrapper"></div>
+      <div className="cart-wrapper">
+        <h3>Items added to cart:</h3>
+        <div className="cart-items">{cart}</div>
+      </div>
       <ul className="product-list">{items}</ul>
     </React.Fragment>
   );
 };
 export default ProductList;
+
+// add items to cart and see what was added
+// make items removable from cart
