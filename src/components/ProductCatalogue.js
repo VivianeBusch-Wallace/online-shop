d7332d4d705e9ac3e65c5a939b37b6512a1ef7f0
viviane <v.buschwallace@yahoo.com>
2021-06-14 14:34:32 +0200

    changes in productcatalogue

--- src/components/ProductCatalogue.js
+++ src/components/ProductCatalogue.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import ProductItem from "./ProductItem";
 
-const ProductList = () => {
+const ProductList = ({ data }) => {
+  // map through json data >>
+  const items = data.map((item, i) => (
+    <ProductItem key={i} productInfo={item} />
+  ));
+
   return (
     <React.Fragment>
-      <div className="catalogue-wrapper"></div>
+      <div className="cart-wrapper"></div>
+      <ul className="product-list">{item}</ul>
     </React.Fragment>
   );
 };
