473d9b93582b740eccd5912146abebbe4118ffc1
viviane <v.buschwallace@yahoo.com>
2021-06-14 12:10:04 +0200

    handleChange and handleSubmit in App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import Data from "./data.json";
 import ProductList from "./components/ProductCatalogue";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+
+  // when the user types into searchbar >>
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  // on submit >>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
       <h1>Spend some money in my store</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} value={userInput} />
-        <input type="submit" value="filter" />
+        <input type="submit" value="find" />
         <ProductList data={userInput ? filteredData : data} />
       </form>
     </div>
