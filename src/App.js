6e134c80fb61e1a2881c7a0d71e59e6cbfa91d9a
viviane <v.buschwallace@yahoo.com>
2021-06-14 12:02:19 +0200

    created return of App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import Data from "./data.json";
 import ProductList from "./components/ProductCatalogue";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Spend some money in my store</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={userInput} />
+        <input type="submit" value="filter" />
+        <ProductList data={userInput ? filteredData : data} />
+      </form>
+    </div>
+  );
 }
 
 export default App;
