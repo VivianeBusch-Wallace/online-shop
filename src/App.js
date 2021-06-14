d24c4501cf9b6d8348a27fcf104e107787ad08b3
viviane <v.buschwallace@yahoo.com>
2021-06-14 13:47:01 +0200

    comments and small changes in App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Data from "./data.json";
 import ProductList from "./components/ProductCatalogue";
 
 function App() {
+  // useState >>
   const [userInput, setUserInput] = useState("");
+  const [data, setData] = useState(Data);
+  // << initialize state with Data and send data to child
 
   // when the user types into searchbar >>
   const handleChange = (e) => {
     setUserInput(e.target.value);
   };
+  // function for filtering by searchbar input >>
 
   // on submit >>
   const handleSubmit = (e) => {
@@ -17,14 +21,24 @@ function App() {
 
   return (
     <div className="App">
+      {
+        // for having homepage and not seeing images do this >>
+        //<img src={`${process.env.PUBLIC_URL}/${data[0].img}`} alt="Sushi" />
+        // fontawesome icons and features >>
+        //<FontAwesomeIcon icon={["fab", "apple"]} size="6x" rotation={90} />
+      }
       <h1>Spend some money in my store</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} value={userInput} />
         <input type="submit" value="find" />
-        <ProductList data={userInput ? filteredData : data} />
       </form>
+      <ProductList data={data} />
     </div>
   );
 }
 
 export default App;
+
+// Do Not forget:
+//         <ProductList data={userInput ? filteredData : data} />
+// filter function
