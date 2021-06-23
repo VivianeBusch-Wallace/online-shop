9927305b85e8aa4d0facf99a5d325f6c82c3961e
viviane <v.buschwallace@yahoo.com>
2021-06-23 11:18:42 +0200

    commented out old way of productcatalogue

--- src/components/ProductCatalogue.js
+++ src/components/ProductCatalogue.js
@@ -1,36 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import ProductItem from "./ProductItem";
 
 const ProductList = ({ data }) => {
+  // old way:
   // states >>
-  const [totalBill, setTotalBill] = useState(0);
-  const [cart, setCart] = useState([]);
+  // const [totalBill, setTotalBill] = useState(0);
+  // const [cart, setCart] = useState([]);
 
   // add each item to cart with click >>
-  const addToCart = (item) => {
-    setCart((prevState) => [...prevState, item]);
-  };
+  // const addToCart = (item) => {
+  //   setCart((prevState) => [...prevState, item]);
+  // };
+  // << this still has issues and does not work properly
 
   // map through json data to get each item >>
-  const items = data.map((item, i) => (
-    <ProductItem key={i} productInfo={item} addToCart={addToCart} />
-  ));
+  // const items = data.map((item, i) => (
+  //   <ProductItem key={i} productInfo={item} addToCart={addToCart} />
+  // ));
 
   // calculate the total bill on click >>
-  const calculateTotal = () => {
-    setTotalBill(cart.reduce((acc, cur) => acc + cur.price, 0));
-  };
+  // const calculateTotal = () => {
+  //   setTotalBill(cart.reduce((acc, cur) => acc + cur.price, 0));
+  // };
   console.log({ cart });
   return (
-    <React.Fragment>
-      <div className="cart-wrapper">
-        <h3>Items added to cart:</h3>
-        {/* <div className="cart-items">{cart.join(", ")}</div> */}
-        <button onClick={calculateTotal}>Total bill</button>
-        <span> (excl. delivery fees): {totalBill} Euros</span>
-      </div>
-      <ul className="product-list">{items}</ul>
-    </React.Fragment>
+
+    // old way >>
+    // <React.Fragment>
+    //   <div className="cart-wrapper">
+    //     <h3>Items added to cart:</h3>
+    //     <div className="cart-items">{cart.join(", ")}</div>
+    //     <button onClick={calculateTotal}>Total bill</button>
+    //     <span> (excl. delivery fees): {totalBill} Euros</span>
+    //   </div>
+    //   <ul className="product-list">{items}</ul>
+    // </React.Fragment>
   );
 };
 export default ProductList;
@@ -38,3 +42,6 @@ export default ProductList;
 // add items to cart and see what was added
 // make items removable from cart
 // why not all visible when cart printed out in console
+
+// issues
+// the cart only prints out [object Object]
